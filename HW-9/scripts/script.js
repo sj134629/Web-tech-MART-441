19ef6237ef4d8d46cc02c9e6466efb25faf7828b
Samantha Jacobson <46580649+sj134629@users.noreply.github.com>
2021-03-14 22:40:06 -0600

    HW-9 done!

--- HW-9/scripts/script.js
+++ HW-9/scripts/script.js
@@ -1,5 +1,25 @@
 $(document).ready(function() {
+  $.fn.color = function(){
+    this.css("margin", "100px");
+    this.css("width", "50%");
+    this.css("background-color", "red");
+    this.css("border-radius", "15px");
+    this.css("margin", "50px");
+
+    this.css("text-align", "center");
+
+
+  };
+  $.fn.textStuff = function(){
+    this.css("color", "white");
+  };
+
+
   $("button").click(function () {
+
+
+         $("#changeDiv").color();
+         $("#changeText").textStuff();
   $.getJSON("./data/info.json", function(data){
     var heroData = '';
     $.each(data, function(key, value){
@@ -9,7 +29,7 @@ $(document).ready(function() {
       heroData += '<td>'+value.city+'</td>';
       heroData += '<td>'+value.strength+'</td>';
       heroData += '</tr>';
-    
+
 
     });
     $('#heroesTable').append(heroData);
