cfacac4c06e92329f5b7ee73585f0692db9b6e99
Samantha Jacobson <46580649+sj134629@users.noreply.github.com>
2021-03-14 20:53:04 -0600

    changed it up

--- HW-9/scripts/script.js
+++ HW-9/scripts/script.js
@@ -1,43 +1,18 @@
-let history =   {
-    "date": "-300",
-    "description": "Pilgrims travel to the healing temples of Asclepieion to be cured of their ills. After a ritual purification the followers bring offerings or sacrifices.",
-    "lang": "en",
-  },
-  "categories" : [
-    "By place",
-    "Greece"
-  ]
-},
+$(document).ready(function() {
+  $("button").click(function () {
+  $.getJSON("./data/info.json", function(data){
+    var heroData = '';
+    $.each(data, function(key, value){
+      heroData += '<tr>';
+      heroData += '<td>'+value.name+'</td>';
+      heroData += '<td>'+value.superheroName+'</td>';
+      heroData += '<td>'+value.city+'</td>';
+      heroData += '<td>'+value.strength+'</td>';
+      heroData += '</tr>';
+    
 
-  {
-    "date": "-300",
-    "description": "Pyrrhus, the King of Epirus, is taken as a hostage to Egypt after the Battle of Ipsus and makes a diplomatic marriage with the princess Antigone, daughter of Ptolemy and Berenice.",
-    "lang": "en",
-  },
-    "categories" : [
-       "By place",
-        "Egypt"
-      ]
-},
-{
-  "date": "-300",
-  "description": "Ptolemy concludes an alliance with King Lysimachus of Thrace and gives him his daughter Arsinoe II in marriage.",
-  "lang": "en",
-},
-  "categories" : [
-     "By place",
-     "Egypt"
-    ]
-}
-
-$(function () {
-            $("button").click(function () {
-                HeroInfo();
-            });
-
-        });
-
-function HeroInfo()
-{
-  $("#HeroInfo").html("Date: " + history.date + "<br>Description: " + history.description + "<br>Language: " + history.lang + "<br>Categories:<br>" + history.categories[0] + "<br>" + history.categories[1]);
-}
+    });
+    $('#heroesTable').append(heroData);
+  });
+});
+});
