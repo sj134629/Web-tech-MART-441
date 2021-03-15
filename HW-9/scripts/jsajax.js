20d3f8bdb18ced7d4c35ebdca4935dc9e86a13c4
Samantha Jacobson <46580649+sj134629@users.noreply.github.com>
2021-03-14 18:20:29 -0600

    Help!!

---
+++ HW-9/scripts/jsajax.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+            $("button").click(function () {
+                $("#HeroInfo").load("./data/info.json", function(responseText){
+                  var heros = JSON.parse(responseText);
+                  $("#HeroInfo").html("Date: " + history.date + "<br>Description: " + history.description + "<br>Language: " + history.lang + "<br>Categories:<br>" + history.categories[0] + "<br>" + history.categories[1]);
+
+                     alert(responseText);
+                });
+            });
+        });
