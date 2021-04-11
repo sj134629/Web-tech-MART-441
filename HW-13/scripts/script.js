5eea3b87155ef6746be6214bd0c47bc468c0683d
Samantha Jacobson <46580649+sj134629@users.noreply.github.com>
2021-04-10 19:08:09 -0600

    model code?

--- HW-13/scripts/script.js
+++ HW-13/scripts/script.js
@@ -1,69 +1,10 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-var cube, cube2;
-
-// create the first box
-function createBox() {
-  // create a box
-  var geometry = new THREE.BoxGeometry();
-  var material = new THREE.MeshBasicMaterial({
-    color: 0x324ca8
-  });
-  cube = new THREE.Mesh(geometry, material);
-  cube.position.set(50, 0, 0);
-  scene.add(cube);
-  cube.scale.x = 15; // SCALE
-  cube.scale.y = 15; // SCALE
-  cube.scale.z = 15; // SCALE
-
-
-  animate();
-}
-
-// animate the first box
-function animate() {
-  requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-  createBox2();
-  renderer.render(scene, camera);
-
-}
-
-
-// create the second box and add it as a child of the first box
-function createBox2() {
-  // create a box
-  var geometry = new THREE.BoxGeometry();
-  var material = new THREE.MeshBasicMaterial({
-    color: 0x1234ee
-  });
-  cube2 = new THREE.Mesh(geometry, material);
-  cube2.position.set(2, 0);
-  cube.add(cube2);
-  cube2.scale.x = .5; // SCALE
-  cube2.scale.y = .5; // SCALE
-  cube2.scale.z = .5; // SCALE
-
-  animate2();
-}
-
-
-function animate2() {
-  requestAnimationFrame(animate2);
-  cube2.rotation.x += 0.05;
-  cube2.rotation.y += 0.05;
-
-
-}
-
 /**
  * Generate a scene object with a background color
  **/
 
 function getScene() {
   var scene = new THREE.Scene();
-  scene.background = new THREE.Color(0xaaaaaa);
+  scene.background = new THREE.Color(0x111111);
   return scene;
 }
 
@@ -110,9 +51,7 @@ function getLight(scene) {
 
 function getRenderer() {
   // Create the canvas with a renderer
-  var renderer = new THREE.WebGLRenderer({
-    antialias: true
-  });
+  var renderer = new THREE.WebGLRenderer({ antialias: true });
   // Add support for retina displays
   renderer.setPixelRatio(window.devicePixelRatio);
   // Specify the size of the canvas
@@ -136,16 +75,19 @@ function getControls(camera, renderer) {
 }
 
 /**
- * Load Skull model
+ * Load Nimrud model
  **/
 
 function loadModel() {
   var loader = new THREE.OBJLoader();
-  loader.load('models/Cerberus.obj', function (object) {
-    object.rotation.z = Math.PI;
-    scene.add(object);
-
-  });
+  loader.load(
+    "./models/Cerberus.obj",
+    function (object) {
+      object.rotation.z = Math.PI;
+      scene.add(object);
+      document.querySelector("h1").style.display = "none";
+    }
+  );
 }
 
 /**
@@ -156,15 +98,14 @@ function render() {
   requestAnimationFrame(render);
   renderer.render(scene, camera);
   controls.update();
-};
+}
 
 var scene = getScene();
 var camera = getCamera();
 var light = getLight(scene);
 var renderer = getRenderer();
 var controls = getControls(camera, renderer);
-var game1 = createBox();
 
-loadModel()
+loadModel();
 
 render();
